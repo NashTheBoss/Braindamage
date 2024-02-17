a317cfd973da1795339f5ab6b762d476014f5f39
Nash <itznash1234@gmail.com>
2024-02-18 00:45:48 +0100

    update

--- GTA/GTA.py
+++ GTA/GTA.py
@@ -9,4 +9,5 @@ while True:
     pygame.event.get
     if pygame.event == quit:
         pygame.quit
+    pygame.draw.circle(screen, (255,0,0), center = (DISPLAY_WIDTH/2 ,DISPLAY_HEIGHT/2), radius = 20)
     pygame.display.update()
