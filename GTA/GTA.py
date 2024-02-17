118c25278f0f8c4ff0a8f189e8fabc3400f827d4
NashTheBoss <160364131+NashTheBoss@users.noreply.github.com>
2024-02-18 00:04:43 +0100

    Add files via upload

---
+++ GTA/GTA.py
@@ -0,0 +1,12 @@
+import pygame
+
+DISPLAY_WIDTH =  1280
+DISPLAY_HEIGHT = 720
+
+screen = pygame.display.set_mode((DISPLAY_WIDTH, DISPLAY_HEIGHT))
+
+while True:
+    pygame.event.get
+    if pygame.event == quit:
+        pygame.quit
+    pygame.display.update()
