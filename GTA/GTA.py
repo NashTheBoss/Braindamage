1b0cc20abc898ebbda8f369c62d9538588d5baf3
LuckyEdosa <itznash1234@gmail.com>
2024-02-18 01:06:42 +0100

    Hallo Nick

--- GTA/GTA.py
+++ GTA/GTA.py
@@ -4,10 +4,15 @@ DISPLAY_WIDTH =  1280
 DISPLAY_HEIGHT = 720
 
 screen = pygame.display.set_mode((DISPLAY_WIDTH, DISPLAY_HEIGHT))
-
+x=0
+def bewegung():
+    global x
+    x=x+1
+#gemeloope
 while True:
     pygame.event.get
     if pygame.event == quit:
         pygame.quit
-    pygame.draw.circle(screen, (255,0,0), center = (DISPLAY_WIDTH/2 ,DISPLAY_HEIGHT/2), radius = 20)
+    pygame.draw.circle(screen, (255,0,0), center = (x ,DISPLAY_HEIGHT/2), radius = 20)
+    bewegung()
     pygame.display.update()
